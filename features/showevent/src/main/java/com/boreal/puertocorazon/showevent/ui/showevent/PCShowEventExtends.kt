49c5e83905cbc907c496ab430594f4494712bc5c
Bau <baudelio_andalon@hotmail.com>
2022-04-15 21:30:58 -0500

    [FEATURE] Bau
    * Working on show event

--- features/showevent/src/main/java/com/boreal/puertocorazon/showevent/ui/showevent/PCShowEventExtends.kt
+++ features/showevent/src/main/java/com/boreal/puertocorazon/showevent/ui/showevent/PCShowEventExtends.kt
@@ -1,12 +1,42 @@
 package com.boreal.puertocorazon.showevent.ui.showevent
 
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import com.boreal.commonutils.extensions.setOnSingleClickListener
+import com.boreal.puertocorazon.showevent.R
 
 fun PCShowEventFragment.initElements() {
-
     binding.apply {
+        navigation()
         btnClose.setOnSingleClickListener {
             requireActivity().onBackPressed()
+        }
+
+        btnGallery.setOnSingleClickListener {
+            navController.navigate(
+                R.id.PCShowEventGalleryFragment,
+            )
+        }
+    }
+
+}
+
+fun PCShowEventFragment.navigation() {
+    binding.apply {
+        navController =
+            (childFragmentManager.findFragmentById(R.id.containerShowEvent) as NavHostFragment).navController
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.PCShowEventDescriptionFragment -> {
+                }
+                R.id.PCShowEventGalleryFragment -> {
+                }
+                R.id.PCShowEventPackagesFragment -> {
+                }
+                else -> {
+
+                }
+            }
         }
     }
 
--- features/showevent/src/main/java/com/boreal/puertocorazon/showevent/ui/showevent/PCShowEventFragment.kt
+++ features/showevent/src/main/java/com/boreal/puertocorazon/showevent/ui/showevent/PCShowEventFragment.kt
@@ -1,6 +1,7 @@
 package com.boreal.puertocorazon.showevent.ui.showevent
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import com.boreal.commonutils.base.CUBaseFragment
 import com.boreal.puertocorazon.showevent.R
 import com.boreal.puertocorazon.showevent.databinding.PcShowEventFragmentBinding
@@ -8,6 +9,9 @@ import com.boreal.puertocorazon.showevent.databinding.PcShowEventFragmentBinding
 class PCShowEventFragment :
     CUBaseFragment<PcShowEventFragmentBinding>() {
     //    PCShowEventViewModel
+
+    lateinit var navController: NavController
+
     override fun getLayout() = R.layout.pc_show_event_fragment
 
     override fun initDependency(savedInstanceState: Bundle?) {
