00fde668e683b72725e6079c433a4038cce7ce35
Bau <baudelio_andalon@hotmail.com>
2022-04-24 17:58:54 -0500

    [FEATURE] Bau
    * Working on add event

--- features/adm/addevent/src/main/java/com/boreal/puertocorazon/addevent/ui/packages/PCPackagesAddEventExtends.kt
+++ features/adm/addevent/src/main/java/com/boreal/puertocorazon/addevent/ui/packages/PCPackagesAddEventExtends.kt
@@ -9,18 +9,37 @@ import com.boreal.puertocorazon.core.utils.formatCurrency
 fun PCPackagesAddEventFragment.initElements() {
     binding.apply {
         initAdapter()
-
+        var priceTempAdult = 0L
+        var priceTempChild = 0L
+        if (viewModel.isPriceAdultValid()) {
+            tvPriceAdultTicket.text = viewModel.getPriceAdult().formatCurrency()
+        }
+        if (viewModel.isPriceChildValid()) {
+            tvPriceChildTicket.text = viewModel.getPriceChildren().formatCurrency()
+        }
         btnTicketAdult.setOnSingleClickListener {
-            PCAddPriceTicket ("Adulto"){ priceTicket ->
+            PCAddPriceTicket("Adulto") { priceTicket ->
                 tvPriceAdultTicket.text = priceTicket.toLong().formatCurrency()
+                priceTempAdult = priceTicket.toLong()
+                tvErrorMessage.text = ""
             }.show(getSupportFragmentManager(), "odmod")
         }
 
         btnTicketChild.setOnSingleClickListener {
             PCAddPriceTicket("Infantil") { priceTicket ->
                 tvPriceChildTicket.text = priceTicket.toLong().formatCurrency()
-
+                priceTempChild = priceTicket.toLong()
             }.show(getSupportFragmentManager(), "odmod")
+        }
+
+        btnSave.setOnSingleClickListener {
+            if (priceTempAdult != 0L) {
+                viewModel.setPriceAdult(priceTempAdult)
+                viewModel.setPriceChildren(priceTempChild)
+                onFragmentBackPressed()
+            } else {
+                tvErrorMessage.text = "Agrega precio al boleto"
+            }
         }
     }
 }
--- features/adm/addevent/src/main/java/com/boreal/puertocorazon/addevent/viewmodel/AddEventViewModel.kt
+++ features/adm/addevent/src/main/java/com/boreal/puertocorazon/addevent/viewmodel/AddEventViewModel.kt
@@ -24,10 +24,22 @@ class AddEventViewModel : CUBaseViewModel() {
         newEvent.mainImageUrl = mainImage
     }
 
+    fun setPriceChildren(price: Long) {
+        newEvent.priceChild = price
+    }
+
+    fun setPriceAdult(price: Long) {
+        newEvent.priceAdult = price
+    }
+
     fun getEventTitle() = newEvent.title
     fun getEventSubtitle() = newEvent.subtitle
     fun getEventDescription() = newEvent.description
     fun getGallery() = newEvent.imageGallery
     fun getMainImage() = newEvent.mainImageUrl
+    fun getPriceAdult() = newEvent.priceAdult
+    fun getPriceChildren() = newEvent.priceChild
+    fun isPriceAdultValid() = newEvent.priceAdult != 0L
+    fun isPriceChildValid() = newEvent.priceChild != 0L
 
 }
