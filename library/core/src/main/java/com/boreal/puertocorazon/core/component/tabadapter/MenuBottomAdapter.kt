8a85f224103054ac1d19b1ed7212c49e285ea020
Bau <baudelio_andalon@hotmail.com>
2022-05-31 15:59:29 -0500

    [FEATURE] Bau
    * Working on Splash

--- library/core/src/main/java/com/boreal/puertocorazon/core/component/tabadapter/MenuBottomAdapter.kt
+++ library/core/src/main/java/com/boreal/puertocorazon/core/component/tabadapter/MenuBottomAdapter.kt
@@ -4,19 +4,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
-class MenuBottomAdapter(private val fragments: ArrayList<Fragment>, fragmentManager: FragmentManager) :
+class MenuBottomAdapter(
+    private val fragments: ArrayList<Fragment>,
+    fragmentManager: FragmentManager
+) :
     FragmentStatePagerAdapter(
         fragmentManager,
         BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
     ) {
-    //arreglo de fragmentos
-
     //si son pocos recursos por cada fragments FragmentSpaceAdapter todos los fragment los carga en memoria
-
     //si requieren de muchos recursos es el fragmentStateAdapter destruye los fragmentos cuando no se estan ocupando
-
-    override fun getCount(): Int = fragments.size
-
-    override fun getItem(position: Int): Fragment = fragments[position]
-
+    override fun getCount() = fragments.size
+    override fun getItem(position: Int) = fragments[position]
 }
